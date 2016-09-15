9cf4522b7d1545ded489c1fcf063477809f5c941
Sushiy <julivizi@yahoo.de>
2016-09-15 14:58:42 +0200

    copied over

---
+++ Assets/EmitterScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EmitterScript : MonoBehaviour
+{
+
+    public SteamVR_Controller.DeviceRelation deviceRelation = SteamVR_Controller.DeviceRelation.Rightmost;
+    public ParticleSystem particles;
+    private int deviceIndex = -1;
+	// Use this for initialization
+	void Start ()
+    {
+        deviceIndex = SteamVR_Controller.GetDeviceIndex(deviceRelation);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(deviceIndex == -1)
+            deviceIndex = SteamVR_Controller.GetDeviceIndex(deviceRelation);
+
+        if (deviceIndex != -1)
+        {
+
+            if (SteamVR_Controller.Input(deviceIndex).GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                SteamVR_Controller.Input(deviceIndex).TriggerHapticPulse(1000);
+                particles.Play();
+            }
+            else if (SteamVR_Controller.Input(deviceIndex).GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                particles.Stop();
+            }
+
+            if (SteamVR_Controller.Input(deviceIndex).GetPressDown(SteamVR_Controller.ButtonMask.Grip))
+            {
+                particles.Clear();
+            }
+
+        }
+        
+	}
+}
---
+++ Assets/MagicTypeChooser.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MagicTypeChooser : MonoBehaviour
+{
+
+    /*
+     * This Script is controlled with the offhand.
+     * It controls the color/type of magic you will be casting. The Type will be shown in the form of a magicsphere that floats on top of the controller.
+     * For now this will be done by clicking the correspondingly colored Button on the trackpad. Controller gestures could later replace this.
+     * 
+     * This Script also transfers the magictype to the mainhand, as soon as the mainhand controller enters the magic sphere.
+     */
+
+    public GameObject m_goMainHandController; //The gameobject of the mainhandcontroller or in this case the tip of the wand controlled with the mainhand.
+
+    private int m_iDeviceIndexThis = -1; //The deviceindex of the offhandcontroller. Is calculated at the beginning of the game.
+
+    
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+
+    public void OnTriggerEnter(Collider _coll)
+    {
+        if(_coll.CompareTag(m_goMainHandController.tag))
+        {
+            Debug.Log("Wand has entered the magic sphere. Now transferring power");
+
+        }
+    }
+}
