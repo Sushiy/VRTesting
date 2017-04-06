58df1d4d171a857888421913d9ff97f094ba865e
Sushiy <julivizi@yahoo.de>
2017-04-06 17:57:21 +0200

    Finished Basic Networkprototype

---
+++ Assets/MultiplayerDEMO/MP_Bullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MP_Bullet : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject hit = collision.gameObject;
+        MP_Health health = hit.GetComponent<MP_Health>();
+        if (health != null)
+        {
+            health.TakeDamage(10);
+        }
+
+        Destroy(gameObject);
+    }
+
+}
