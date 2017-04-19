abeaeca1042595da450242355eb792a59d4bad48
rayvshimself <m.ray.varkki@gmail.com>
2017-04-19 18:04:44 +0200

    Imported TestCharacter IKs dont work yet.
    Test Charakter importiert, jedoch funktionieren die IKs noch nicht in
    Unity. Kümmere mich noch darum.

---
+++ Assets/BasicArena/Scripts/IKHandling.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IKHandling : MonoBehaviour {
+
+    Animator anim;
+
+    public float ikWeight = 1;
+
+    public Transform leftIKTarget;
+    public Transform rightIKTarget;
+
+
+
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    void OnAnimatorIK()
+    {
+        anim.SetIKPositionWeight(AvatarIKGoal.LeftHand, ikWeight);
+        anim.SetIKPositionWeight(AvatarIKGoal.RightHand, ikWeight);
+
+        anim.SetIKPosition(AvatarIKGoal.LeftHand, leftIKTarget.position);
+        anim.SetIKPosition(AvatarIKGoal.RightHand, rightIKTarget.position);
+    }
+}
