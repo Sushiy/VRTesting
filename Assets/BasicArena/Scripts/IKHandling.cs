683eabeb0718228cbf205f5e1d6eb0efbe12a814
rayvshimself <m.ray.varkki@gmail.com>
2017-04-21 17:21:57 +0200

    Made some progress on IKs, made shitty script less shitty

--- Assets/BasicArena/Scripts/IKHandling.cs
+++ Assets/BasicArena/Scripts/IKHandling.cs
@@ -4,35 +4,42 @@ using UnityEngine;
 
 public class IKHandling : MonoBehaviour {
 
-    Animator anim;
+    Animator avatar;
 
-    public float ikWeight = 1;
+    public float leftIKWeight = 1;
+    public float rightIKWeight = 1;
+
 
     public Transform leftIKTarget;
     public Transform rightIKTarget;
 
 
 
-
-
-
     // Use this for initialization
     void Start () {
-		
-	}
+
+       avatar = GetComponent<Animator>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
 
-
     void OnAnimatorIK()
     {
-        anim.SetIKPositionWeight(AvatarIKGoal.LeftHand, ikWeight);
-        anim.SetIKPositionWeight(AvatarIKGoal.RightHand, ikWeight);
+        if (avatar)
+        {
+            avatar.SetIKPositionWeight(AvatarIKGoal.LeftHand, leftIKWeight);
+            avatar.SetIKPositionWeight(AvatarIKGoal.RightHand, rightIKWeight);
+            avatar.SetIKWeight(AvatarIKGoal.LeftHand, leftIKWeight);
+            avatar.SetIKWeight(AvatarIKGoal.RightHand, rightIKWeight);
 
-        anim.SetIKPosition(AvatarIKGoal.LeftHand, leftIKTarget.position);
-        anim.SetIKPosition(AvatarIKGoal.RightHand, rightIKTarget.position);
+            avatar.SetIKPosition(AvatarIKGoal.LeftHand, leftIKTarget.position);
+            avatar.SetIKPosition(AvatarIKGoal.RightHand, rightIKTarget.position);
+
+            avatar.SetIKRotation(AvatarIKGoal.LeftHand, leftIKTarget.rotation);
+            avatar.SetIKRotation(AvatarIKGoal.RightHand, rightIKTarget.rotation);
+        }
     }
 }
