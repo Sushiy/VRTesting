83095885041566ab98efdf141cad88d661e0fb77
rayvshimself <m.ray.varkki@gmail.com>
2017-05-03 14:50:17 +0200

    Character IKs are wokring; worked on levellayout
    Inserted Idle Animaiton in Mecanim, to stop the character falling a few
    Units into the floor.
    
    worked on Level Layout. it is a bit more Spcious now and the hierarchy
    is cleaner.

--- Assets/BasicArena/Scripts/IKHandling.cs
+++ Assets/BasicArena/Scripts/IKHandling.cs
@@ -2,43 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IKHandling : MonoBehaviour {
+public class IKHandling : MonoBehaviour
+{
 
     Animator avatar;
 
     public float leftIKWeight = 1;
     public float rightIKWeight = 1;
 
-
+    // insert Object to target IKs
     public Transform leftIKTarget;
     public Transform rightIKTarget;
 
 
 
-    // Use this for initialization
-    void Start () {
+    void Start ()
+    {
 
        avatar = GetComponent<Animator>();
     }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
     void OnAnimatorIK()
     {
         if (avatar)
         {
+            // Setting Weights of IK Limbs
             avatar.SetIKPositionWeight(AvatarIKGoal.LeftHand, leftIKWeight);
-            avatar.SetIKPositionWeight(AvatarIKGoal.RightHand, rightIKWeight);
             avatar.SetIKRotationWeight(AvatarIKGoal.LeftHand, leftIKWeight);
+
+            avatar.SetIKPositionWeight(AvatarIKGoal.RightHand, rightIKWeight);
             avatar.SetIKRotationWeight(AvatarIKGoal.RightHand, rightIKWeight);
 
+            // Setting target for IK Limbs
             avatar.SetIKPosition(AvatarIKGoal.LeftHand, leftIKTarget.position);
-            avatar.SetIKPosition(AvatarIKGoal.RightHand, rightIKTarget.position);
-
             avatar.SetIKRotation(AvatarIKGoal.LeftHand, leftIKTarget.rotation);
+
+            avatar.SetIKPosition(AvatarIKGoal.RightHand, rightIKTarget.position);        
             avatar.SetIKRotation(AvatarIKGoal.RightHand, rightIKTarget.rotation);
         }
     }
