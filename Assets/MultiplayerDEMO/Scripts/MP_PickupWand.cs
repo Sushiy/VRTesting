1a98170c9add62837108a35b1113ec3d47fac05d
Vivien <hollowspecder@googlemail.com>
2017-05-15 14:49:36 +0200

    made it so the wand has to be picked up first

---
+++ Assets/MultiplayerDEMO/Scripts/MP_PickupWand.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MP_PickupWand : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_prefabWand;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Hand"))
+        {
+            Instantiate<GameObject>(m_prefabWand, other.transform);
+            Destroy(gameObject);
+        }
+    }
+}
