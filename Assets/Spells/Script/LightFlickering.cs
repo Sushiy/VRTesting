0826ea1596bdc67da34b60fcaf3c36aa661951b5
rayvshimself <m.ray.varkki@gmail.com>
2017-05-26 15:43:29 +0200

    Added a new Spell effect. (called "Spheri" atm)

--- Assets/Spells/Script/LightFlickering.cs
+++ Assets/Spells/Script/LightFlickering.cs
@@ -6,16 +6,12 @@ public class LightFlickering : MonoBehaviour
 	private Light lt;
 	public Color lightColor = Color.red;
 
-	public float minIntensity = 1.6f;
-	public float Intensity = 1.5f;
-	public float intSpeed = 1f;
-
+	public float minIntensity = 1.5f;
+	public float maxIntensity = 1.6f;
 
 	public float minRange = 4f;
     public float Range = 0.4f;
-	public float rangSpeed = 1f;
-
-
+	public float maxRange = 4.4f;
 
 	void Start()
     {
@@ -23,8 +19,8 @@ public class LightFlickering : MonoBehaviour
 	}
 	void FixedUpdate()
     {
-		lt.intensity = minIntensity + Mathf.PingPong(Time.time * intSpeed, Intensity);
-		lt.range =  minRange + Mathf.PingPong(Time.time * rangSpeed, Range);
+		lt.intensity = minIntensity + Mathf.PingPong(Time.time * Mathf.Round(Random.Range(minRange, maxRange)), maxIntensity);
+		lt.range =  minRange + Mathf.PingPong(Time.time * Mathf.PingPong(Time.time, maxIntensity), Range);
 		lt.color = lightColor;
 	}
 }
