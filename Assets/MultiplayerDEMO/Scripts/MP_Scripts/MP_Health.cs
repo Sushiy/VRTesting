6ad6d4bc47d17107bea4350a10ea525c7502d7f1
Sushiy <julivizi@yahoo.de>
2017-06-26 21:06:46 +0200

    fix the health bar once and for all?

--- Assets/MultiplayerDEMO/Scripts/MP_Scripts/MP_Health.cs
+++ Assets/MultiplayerDEMO/Scripts/MP_Scripts/MP_Health.cs
@@ -51,8 +51,8 @@ public class MP_Health : NetworkBehaviour
     void OnChangeHealth(int currentHealth)
     {
         this.currentHealth = currentHealth;
-        old_Healthbar.sizeDelta = new Vector2(currentHealth, old_Healthbar.sizeDelta.y);
-        m_healthbar.UpdateHealth(currentHealth);
+        //old_Healthbar.sizeDelta = new Vector2(currentHealth, old_Healthbar.sizeDelta.y);
+        //m_healthbar.UpdateHealth(currentHealth);
     }
 
     [ClientRpc]
