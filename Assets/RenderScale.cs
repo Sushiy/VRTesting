8c18d27eef292a08aa01c563992631b3ee440e5d
VRRechnerRechts <baguiov@posteo.net>
2017-09-28 15:38:47 +0200

    settings and added renderscale scripty

---
+++ Assets/RenderScale.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderScale : MonoBehaviour
+{
+
+    [SerializeField] private float m_RenderScale = 1f;              //The render scale. Higher numbers = better quality, but trades performance
+
+    void Start()
+    {
+        VRSettings.renderScale = m_RenderScale;
+    }
+}
